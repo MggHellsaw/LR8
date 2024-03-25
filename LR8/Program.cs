9789dd4cb0bcbc465de15f05e45005f1d9a0f1d2
MggHellsaw <92123919+MggHellsaw@users.noreply.github.com>
2024-03-25 23:19:25 +0200

    Program.cs

--- LR8/Program.cs
+++ LR8/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews(); // Додаємо підтримку контролерів та представлень
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -9,7 +9,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,11 +21,11 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"); // Налаштовуємо маршрутизацію для контролерів та дій
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllers(); // Додаємо мапінг для контролерів
+    endpoints.MapControllers();
 });
 
 app.Run();
